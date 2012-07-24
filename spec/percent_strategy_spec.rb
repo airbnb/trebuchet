723a2bfd60d29b7b92187570ef82202eb8794e9d
Justin Jones <justin@airbnb.com>
2012-07-24 16:49:47 -0700

    new percent specs

--- spec/percent_strategy_spec.rb
+++ spec/percent_strategy_spec.rb
@@ -41,5 +41,69 @@ describe Trebuchet::Strategy::Percentage do
     t = Trebuchet.new User.new(0 - (offset - 1))
     t.launch?('percentage').should === false
   end
+  
+  
+  def offset_ids(ids, offset)
+    ids.to_a.map {|id| id + offset % 100} 
+  end
+  
+  
+  it "should launch to the right users with percentage" do
+    Trebuchet.feature('freedom').aim(:percent, :percentage => 50)
+    offset = Trebuchet.feature('freedom').strategy.offset
+    should_launch 'freedom', offset_ids(0..49, offset)
+    should_not_launch 'freedom',  offset_ids(50..99, offset)
+    should_launch 'freedom',  offset_ids(100..149, offset)
+    should_not_launch 'freedom',  offset_ids(150..199, offset)
+    should_launch'freedom',  offset_ids([200, 201], offset)
+    should_not_launch 'freedom',  offset_ids([250, 251], offset)
+  end
+  
+  it "should launch to the right users with wrap" do
+    Trebuchet.feature('freedom').aim(:percent, :from => 99, :to => 1)
+    should_launch 'freedom', [0, 1, 99, 100, 101, 199, 200, 201, 299]
+    should_not_launch 'freedom', [97, 98, 102, 103, 198, 202, 298, 302]
+  end
+  
+  it "should launch to 1%" do
+    Trebuchet.feature('freedom').aim(:percent, :percentage => 1)
+    offset = Trebuchet.feature('freedom').strategy.offset
+    should_launch 'freedom', offset_ids([100, 200, 300, 0], offset)
+    should_not_launch 'freedom', offset_ids([1, 99, 101, 198, 202, 350], offset)
+  end
+  
+  it "should handle 0%" do
+    Trebuchet.feature('freedom').aim(:percent, 0)
+    should_not_launch 'freedom', (1..200).to_a
+  end
+  
+  it "should handle garbage arguments" do
+    ids = (1..200).to_a
+    Trebuchet.feature('freedom').aim(:percent, -1)
+    should_not_launch 'freedom', ids
+    Trebuchet.feature('freedom').aim(:percent, -150)
+    should_not_launch 'freedom', ids
+    Trebuchet.feature('freedom').aim(:percent, 'h')
+    should_not_launch 'freedom', ids
+    Trebuchet.feature('freedom').aim(:percent, 'h')
+    should_not_launch 'freedom', ids
+    Trebuchet.feature('freedom').aim(:percent, [5, 10])
+    should_not_launch 'freedom', ids
+    Trebuchet.feature('freedom').aim(:percent, 20..50)
+    should_not_launch 'freedom', ids
+    Trebuchet.feature('freedom').aim(:percent, nil)
+    should_not_launch 'freedom', ids
+    Trebuchet.feature('freedom').aim(:percent, :from => 7)
+    should_not_launch 'freedom', ids
+    Trebuchet.feature('freedom').aim(:percent, :to => 1)
+    should_not_launch 'freedom', ids
+  end
+  
+  it "works with from/to > 100" do
+    # just documenting this side-effect
+    Trebuchet.feature('freedom').aim(:percent, :from => 105, :to => 210)
+    should_launch 'freedom', [5, 106, 207, 308, 409, 1210]
+    should_not_launch 'freedom', [4, 99, 111, 150, 215]
+  end
 
 end
