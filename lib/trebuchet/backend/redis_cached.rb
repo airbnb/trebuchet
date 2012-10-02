9826055751f387b7f7109ade0452ca03d090390c
Justin Jones <justin@airbnb.com>
2012-10-02 14:54:17 -0700

    Trebuchet RedisCached backend, uses in-memory caching to reduce load on Redis

---
+++ lib/trebuchet/backend/redis_cached.rb
@@ -0,0 +1,30 @@
+class Trebuchet::Backend::RedisCached < Trebuchet::Backend::Redis
+
+  # cache strategies in memory until clear_cached_strategies is called
+
+  def get_strategy(feature_name)
+    if cached_strategies.has_key?(feature_name)
+      # use cached if available (even if value is nil)
+      cached_strategies[feature_name]
+    else 
+      # or call Trebuchet::Backend::Redis#get_strategy 
+      # which will fetch from Redis and unpack json
+      # and then cache it for next time
+      cache_strategy feature_name, super(feature_name)
+    end
+  end
+
+  def cache_strategy(feature_name, strategy)
+    cached_strategies[feature_name] = strategy
+    return strategy
+  end
+
+  def cached_strategies
+    @cached_strategies ||= Hash.new
+  end
+
+  def clear_cached_strategies
+    @cached_strategies = nil
+  end
+
+end
