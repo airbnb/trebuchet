3d62bd1d2981938699a74de20d5c925cee63e891
Justin Jones <justin@airbnb.com>
2012-12-12 18:36:49 -0800

    Rails 3 style routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do |map|
-  # namespace 'trebuchet' do
+# TrebuchetRails::Engine.routes do
+Rails.application.routes do
   scope "trebuchet", :module => "trebuchet_rails" do
     root :to => "features#index"
     get 'timeline' => "features#timeline"
