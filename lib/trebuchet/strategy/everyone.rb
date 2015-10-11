f7ba606ab78b039cdb3f29c5c7421257a1e755ee
Nick Adams <nick.adams@airbnb.com>
2015-10-11 16:11:19 -0700

    add everyone and nobody strategies to trebuchet and make them singletons

---
+++ lib/trebuchet/strategy/everyone.rb
@@ -0,0 +1,26 @@
+require 'singleton'
+# Everyone is to launch the feature to everyone
+class Trebuchet::Strategy::Everyone < Trebuchet::Strategy::Base
+  include Singleton
+
+  def initialize(options = nil)
+    # ignore options
+  end
+
+  def name
+    :everyone
+  end
+
+  def launch_at?(user, request = nil)
+    true
+  end
+
+  def needs_user?
+    false
+  end
+
+  def to_s
+    "everyone (everyone)"
+  end
+
+end
