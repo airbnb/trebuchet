bd91802d2badcc1115e219f6d7bc5e9754715c24
Daniel Evans <daniel.evans@airbnb.com>
2020-01-02 16:01:45 -0800

    Fix missing state author methods

--- lib/trebuchet/state.rb
+++ lib/trebuchet/state.rb
@@ -1,5 +1,6 @@
 # Represents the internal, global and thread-unsafe state of Trebuchet
 Trebuchet::State = Struct.new(
   :visitor_id, :current, :current_block,
-  :logs, :admin_view, :admin_edit, :time_zone
+  :logs, :admin_view, :admin_edit, :time_zone,
+  :author
 )
